4f8a65a326f01960506acc46a85b0bfa3f08c174
Rohan Shirke <57215607+rohan-shirke@users.noreply.github.com>
2023-09-01 07:46:17 +0530

    Time: 2 ms (99.93%) | Memory: 43.6 MB (79.03%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
+
+        // Traverse from left to right
+        for(int i=1; i<n; i++){
+            if(ratings[i] > ratings[i-1]){
+                candies[i] = candies[i-1]+1;
+            }
+        }
+        // Traverse from right to left
+        for(int i=n-2; i>=0; i--){
+            if(ratings[i]>ratings[i+1] && candies[i] <= candies[i+1]){
+                candies[i] = candies[i+1] +1;
+            }
+        }
+
+        // Calculate the sum of candies
+        int sum = 0;
+
+        for(int candy : candies){
+            sum+=candy;
+        }
+        return sum;
+
+    }
+}
