8ae635bcdfb44882a92966781da07e262bf7f489
Rohan Shirke <57215607+rohan-shirke@users.noreply.github.com>
2023-09-21 08:43:27 +0530

    Time: 9 ms (89.92%), Space: 56 MB (38.95%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,70 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+       if (head == null || head.next == null) {
+            return head; // Base case: list is empty or has only one element
+        }
+
+        // Find the middle of the list
+        ListNode mid = findMiddle(head);
+
+        // Split the list into two halves
+        ListNode left = head;
+        ListNode right = mid.next;
+        mid.next = null; // Break the link between the two halves
+
+        // Recursively sort the two halves
+        left = sortList(left);
+        right = sortList(right);
+
+        // Merge the sorted halves
+        return merge(left, right); 
+    }
+
+    private ListNode findMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    private ListNode merge(ListNode left, ListNode right) {
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                current.next = left;
+                left = left.next;
+            } else {
+                current.next = right;
+                right = right.next;
+            }
+            current = current.next;
+        }
+
+        if (left != null) {
+            current.next = left;
+        }
+
+        if (right != null) {
+            current.next = right;
+        }
+
+        return dummy.next;
+    }
+}
