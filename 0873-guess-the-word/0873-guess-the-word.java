6af06e86f4bf0245e0a1103fca28c25f93da00f6
Rohan Shirke <57215607+rohan-shirke@users.noreply.github.com>
2023-10-03 20:35:33 +0530

    Time: 1 ms (99.22%), Space: 40.1 MB (95.89%) - LeetHub

---
+++ 0873-guess-the-word/0873-guess-the-word.java
@@ -0,0 +1,24 @@
+/**
+ * // This is the Master's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface Master {
+ *     public int guess(String word) {}
+ * }
+ */
+class Solution {
+    public void findSecretWord(String[] words, Master master) {
+        int last = words.length-1;
+			while(true){
+				int count = master.guess(words[0]);
+				if(count==6) return;
+                int i = 1;
+				while(i <= last){
+					int c = 0;
+					for(int j = 0; j < 6; j++) if(words[i].charAt(j)==words[0].charAt(j)) c++;
+					if(c!=count) words[i]=words[last--];
+					else i++;
+				}
+				words[0]=words[last--];
+			}
+    }
+}
