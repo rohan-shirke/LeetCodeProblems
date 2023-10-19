030b652686ecc528b7a4627637b7671bfcf19074
Rohan Shirke <57215607+rohan-shirke@users.noreply.github.com>
2023-10-19 17:46:55 +0530

    Time: 1 ms (86.75%), Space: 40.3 MB (88.11%) - LeetHub

---
+++ 0874-backspace-string-compare/0874-backspace-string-compare.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        return buildString(s).equals(buildString(t));
+    }
+
+    public String buildString(String str){
+        StringBuilder result = new StringBuilder();
+
+        for(char ch : str.toCharArray()){
+            if(ch != '#'){
+                result.append(ch);
+            }else if(result.length() > 0){
+                result.deleteCharAt(result.length()-1);
+            }
+        }
+        return result.toString();
+    }
+}
